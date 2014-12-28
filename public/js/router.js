addd627a245e61e689c2e14fba0ec87a112a8e4a
Will Sutherland <wgpsutherland@gmail.com>
2014-12-28 22:22:22 +0000

    Removed exit page.
    Logout is much simpler, just goes right back to the home page. closes
    #23.

--- public/js/router.js
+++ public/js/router.js
@@ -16,7 +16,6 @@ define([
     'libs/getCookie',
     'collections/InstitutionsCollection',
     'collections/CourseCollection',
-    'views/LoggedOutPage',
     'collections/GradesCollection',
     'views/AddGradePage',
     'collections/AssignmentsCollection',
@@ -25,7 +24,7 @@ define([
 ], function($, _, Backbone, bootstrap, HomePage, ProfilePage, UserCollection,
             SignupPage, LoginCollection, UserModel,
             AddModulePage, ModuleCollection, UserModulesCollection, getCookie, InstitutionsCollection,
-            CourseCollection, LoggedOutPage, GradesCollection, AddGradePage, AssignmentsCollection, AdminPage,
+            CourseCollection, GradesCollection, AddGradePage, AssignmentsCollection, AdminPage,
             EditGradePage) {
 
 	var Router = Backbone.Router.extend({
@@ -38,7 +37,6 @@ define([
             'profile/:id': 'profile',
             'signup': 'signup',
             'profile/:id/addModule': 'addModule',
-            'exit': 'exit',
             'profile/:id/addGrade/:od': 'addGrade',
             'profile/:id/admin': 'admin',
             'profile/:id/editGrade/:ed/:od': 'editGrade'
@@ -92,8 +90,6 @@ define([
             userModulesCollection: userModulesCollection
         });
 
-        var loggedOutPage = new LoggedOutPage();
-
         var addGradePage = new AddGradePage({
             gradesCollection: gradesCollection,
             router: router,
@@ -117,7 +113,6 @@ define([
             profile: profilePage,
             signup: signupPage,
             addModule: addModulePage,
-            exit: loggedOutPage,
             addGrade: addGradePage,
             admin: adminPage,
             editGrade: editGradePage
@@ -125,24 +120,18 @@ define([
 
 		router.on('route', function(pageName, stuff) {
 
-            if((pageName!="exit") && (pageName=="profile" || pageName=="addModule" || pageName=="addGrade" || pageName=="admin" || pageName=="editGrade") && (stuff[0] != $("user_id").getCookie())) { // if the page requires login and the cookie user id is not correct then redirect back to the homepage
+            if((pageName=="profile" || pageName=="addModule" || pageName=="addGrade" || pageName=="admin" || pageName=="editGrade") && (stuff[0] != $("user_id").getCookie())) { // if the page requires login and the cookie user id is not correct then redirect back to the homepage
 
-                console.log("this is happening");
                 router.navigate('#', {trigger: true});
 
-            } else if ((pageName!="exit") && (pageName=="home" || pageName=="signup") && ($("user_id").getCookie() != "")) { // if the user if logged in redirect to the profile
+            } else if ((pageName=="home" || pageName=="signup") && ($("user_id").getCookie() != "")) { // if the user if logged in redirect to the profile
 
                 router.navigate('#/profile/'+$("user_id").getCookie(), {trigger: true});
 
             } else if(pageName=="admin" && $("user_id").getCookie()!=1) { // only certain ids can access the admin page
 
-                console.log("this is happening");
                 router.navigate('#', {trigger: true});
             } else {
-
-                if(pageName=="exit") {
-                    document.cookie = "user_id=";
-                }
 
                 if (!$('.page > [data-name="' + pageName + '"]').length) { // checks if the page has been rendered before
 
